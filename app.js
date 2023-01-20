eab65e92515e0aff076cbd0066cf92a8d812e097
Andre Maurell <amaurell@gmail.com>
2023-01-20 10:58:14 -0300

    adicionei comentarios no app.js

--- app.js
+++ app.js
@@ -4,23 +4,24 @@ const min = 1;
 const max = 10;
 let numero;
 let pegaConteudo;
-let pegaAcerto;
-let pegaErro;
+let msgGanhou;
+let msgErrou;
 let jogarBtn;
-let limpa;
+let limpaInput;
 let tentativas = 3;
 
 
 
 numeroRandom();
 
+// Gera o número aleatório
 function numeroRandom(){
   numero =Math.floor((Math.random()*10)+1);
   console.log(numero);
 }
 
-jogarBtn        = document.querySelector(".submit");
-pegaAcerto   = document.querySelector(".resultado");
+jogarBtn        = document.querySelector(".submit"); 
+msgGanhou       = document.querySelector(".resultado");
 pegaTentativas  = document.querySelector(".tentativas");
 
 document.querySelector(".texto").focus();
@@ -33,32 +34,37 @@ jogarBtn.addEventListener("click",function(){
   
     pegaConteudo=parseInt(document.querySelector(".texto").value);
     
+    // Testa se o campo de digitação está vazio
     if (isNaN(pegaConteudo)==true){
       alert ("Digite o número !!");
     }
    
  
-  
+  // Testa se o número digitado é diferente do gerado randomicamente
   if(numero !== pegaConteudo){
     
     tentativas--;
-    pegaAcerto.textContent = "Resultado: Você Errou!!!!";
-    pegaErro=document.querySelector(".resultado").style.color = "red";
-    limpa=document.querySelector(".texto").value = "";
+
+    msgGanhou.textContent = "Resultado: Você Errou!!!!";
+    msgErrou=document.querySelector(".resultado").style.color = "red";
+    limpaInput=document.querySelector(".texto").value = "";
     document.querySelector(".texto").focus();
     pegaTentativas.textContent = tentativas;
   }
     
-  
+  // Testa se o número digitado é igual do gerado randomicamente
   if(pegaConteudo===numero){
-    pegaAcerto.textContent = "Resultado: Você Ganhou!!!!";
-    pegaAcerto=document.querySelector(".resultado").style.color = "green";
+
+    msgGanhou.textContent = "Resultado: Você Ganhou!!!!";
+    msgGanhou=document.querySelector(".resultado").style.color = "green";
     jogarBtn.disabled=true;
+
   }
 
+  // Testa se o número de tentativas terminou
   if(tentativas ===0){
 
-    pegaAcerto = document.querySelector('.resultado').textContent = "Voce Perdeu!";
+    msgGanhou = document.querySelector('.resultado').textContent = "Voce Perdeu!";
     jogarBtn.disabled=true;
 
   }
